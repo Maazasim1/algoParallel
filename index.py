a55eee5d76c30c6ebb52b2beec0d5c82ab656098
Maaz Asim <58209878+Maazasim1@users.noreply.github.com>
2022-12-09 22:06:58 +0500

    added full parallel bucket sort functionality with per bucket sort and dynamic sizing

--- index.py
+++ index.py
@@ -4,50 +4,48 @@ from flask import request
 import math
 from time import sleep
 from flask_cors import CORS
-from flask import jsonify
 
-arr=[None]*10
 
-def buckerSort(i):
-    
-    #arr[math.floor((x[i]*10))]=x[i]
-    index=math.floor(i*10)
-   # print(index)
-    arr[index]=i
-    
-    
 
-def bucketSortParallel(buckets):
+def bucket_sort(i,array):
+
+    # arr[math.floor((x[i]*10))]=x[i]
+    index = math.floor(i * 10)
+    # print(index)
+    array[index].append(i)
+
+
+def bucket_sort_parallel(buckets,size,array):
+    for i in range(size):
+        array.append([])
     for i in buckets:
         print(i)
-        arg_tuple=(i,)
-        _thread.start_new_thread(buckerSort,arg_tuple)
-    print(arr)
+        arg_tuple = (i,array)
+        _thread.start_new_thread(bucket_sort, arg_tuple)
+    print(array)
 
 
-
-    
-
-  
+    return array
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-@app.route('/',methods = ['POST'])
 
-def getArrayAndSort():
-    array=request.json['array']
-    bucketSortParallel(array)
+@app.route('/', methods=['POST'])
+def get_array_and_sort():
+    array = request.json['array']
+    size=len(array)
+    print(size)
+
+    arr=bucket_sort_parallel(array,size,[])
     sleep(0.001)
-    return jsonify({'sorted':arr})
+    for i in range(size):
+        arr[i].sort()
+    return {'sorted': arr}
 
-
-        
 
 if __name__ == '__main__':
-
     app.run(debug=False)
-
 
 
